13b20eb86bda7199e380680caf480f775cbabea1
Andre Pereira <andre.pds07@gmail.com>
2024-10-03 21:20:07 -0300

    criando o arquivo .gitignore

---
+++ run.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+db = create_engine()
+Session = sessionmaker(bind=db)
+session = Session()
+
+Base = declarative_base()
+
+
+Base.metadata.create_all(bind=db)
